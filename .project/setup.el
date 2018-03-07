8b1a15067306422c95a1c88b1e5f748f50bb4816
ramblehead <v.rybynok@gmail.com>
2018-03-07 00:24:08 +0000

    Checkpoint

--- .project/setup.el
+++ .project/setup.el
@@ -1,9 +1,18 @@
 ;; -*- coding: utf-8 -*-
 
+(set (make-local-variable 'tide-tsserver-executable)
+     (concat (rh-project-get-root) "node_modules/.bin/tsserver"))
+
+(set (make-local-variable 'tern-command)
+     (list (concat (rh-project-get-root) "node_modules/.bin/tern")))
+
+(set (make-local-variable 'rh-tern-argument-hints-enabled) nil)
+
 (let ((project-root (rh-project-get-root))
       file-rpath)
   (when project-root
     (setq file-rpath (file-relative-name buffer-file-name project-root))
-    (cond ((string-match-p "src/.+\\.d\\.ts$" file-rpath) nil)
-          ((string-match-p "src/.+\\.tsx$" file-rpath)
-           (rh-project-setup "tsx-react")))))
+    (cond ((string-match-p "\\.ts\\'\\|\\.tsx\\'" file-rpath)
+           (rh-typescript-setup))
+          ((string-match-p "\\.js\\'" file-rpath)
+           (rh-javascript-setup)))))
