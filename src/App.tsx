22bf1d1f294ed06f184e08dbb46a01272cd1d8de
ramblehead <v.rybynok@gmail.com>
2018-03-11 01:01:04 +0000

    Checkpoint

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,65 @@
 import * as React from 'react';
+import gql from 'graphql-tag';
+import { graphql, ChildProps } from 'react-apollo';
+
 import './App.css';
+
+const HERO_QUERY = gql`
+  query GetCharacter($episode: String!) {
+    hero(episode: $episode) {
+      name
+      id
+      friends {
+        name
+        id
+        appearsIn {
+          title
+        }
+      }
+    }
+  }
+`;
+
+interface AppearsIn {
+  title: string;
+}
+
+interface Hero {
+  name: string;
+  id: string;
+  appearsIn: AppearsIn[];
+  friends: Hero[];
+}
+
+interface Response {
+  hero: Hero;
+}
+
+interface InputProps {
+  episode: string;
+}
+
+const withCharacter = graphql<Response, InputProps>(HERO_QUERY, {
+  options: ({ episode }) => ({
+    variables: { episode }
+  })
+});
+
+class Character extends React.Component<ChildProps<InputProps, Response>, {}> {
+  render() {
+    const data = this.props.data!;
+    const { loading, error } = data;
+
+    if (loading) return <div>Loading</div>;
+    if (error) return <div>Error</div>;
+
+    const hero = data.hero!;
+
+    return <div>{hero.name}</div>;
+  }
+}
+
+let CharacterWithData = withCharacter(Character);
 
 const logo = require('./logo.svg');
 
@@ -14,6 +74,7 @@ class App extends React.Component {
         <p className="App-intro">
           To get started, edit <code>src/App.ts</code> and save to reload.
         </p>
+        <CharacterWithData episode="Awakens" />
       </div>
     );
   }
---
+++ src/declarations.d.ts
@@ -0,0 +1,4 @@
+// -*- coding: utf-8; mode: typescript -*-
+
+// see https://github.com/apollographql/react-apollo/issues/1286
+declare module "lodash.flowright";
