c9858540279c3755b15b47cec15ea413fd52c7c8
ramblehead <v.rybynok@gmail.com>
2018-08-29 23:50:40 +0100

    Adding react-sortable-tree and removing node_modules from tslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,38 @@
-import * as React from 'react';
 // import gql from 'graphql-tag';
+
+import * as React from 'react';
+import { Component } from 'react';
 import { graphql, ChildProps } from 'react-apollo';
 
 import { getElementQueryGql } from './graphql/queries';
 import { GetElementQuery,
          GetElementQueryVariables } from './graphql/queries-types';
 
+import SortableTree from 'react-sortable-tree';
+
+import 'react-sortable-tree/style.css';
 import './App.css';
+
+class Tree extends Component<any, any> {
+  constructor(props: any) {
+    super(props);
+
+    this.state = {
+      treeData: [{ title: 'Chicken', children: [{ title: 'Egg' }] }],
+    };
+  }
+
+  render() {
+    return (
+      <div style={{ height: 400 }}>
+        <SortableTree
+          treeData={this.state.treeData}
+          onChange={treeData => this.setState({ treeData })}
+        />
+      </div>
+    );
+  }
+}
 
 const withElement = graphql<GetElementQuery, GetElementQueryVariables>(
   getElementQueryGql, {
@@ -15,8 +41,8 @@ const withElement = graphql<GetElementQuery, GetElementQueryVariables>(
     })
   });
 
-class Element extends React.Component<ChildProps<GetElementQueryVariables,
-                                                 GetElementQuery>, {}> {
+class Element extends Component<ChildProps<GetElementQueryVariables,
+                                           GetElementQuery>, {}> {
   render() {
     const data = this.props.data!;
     const { loading, error } = data;
@@ -47,6 +73,7 @@ class App extends React.Component {
           To get started, edit <code>src/App.ts</code> and save to reload.
         </p>
         <ElementWithData id="0003" />
+        <Tree />
       </div>
     );
   }
