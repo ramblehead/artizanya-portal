cfac4c324d18aefe92be8930d6f7cc2af3effd85
ramblehead <v.rybynok@gmail.com>
2019-01-07 21:39:33 +0000

    Checkpoint

--- .project/setup.el
+++ .project/setup.el
@@ -3,9 +3,6 @@
 (set (make-local-variable 'tide-tsserver-executable)
      (concat (rh-project-get-root) "node_modules/.bin/tsserver"))
 
-;; (set (make-local-variable 'tern-command)
-;;      (list (concat (rh-project-get-root) "node_modules/.bin/tern")))
-
 (set (make-local-variable 'flycheck-typescript-tslint-executable)
      (concat (rh-project-get-root) "node_modules/.bin/tslint"))
 
@@ -20,8 +17,12 @@
                                              (goto-char (point-min))
                                              (thing-at-point 'line t)))
                (setq ext-js (string-match-p "\\.js\\'" file-rpath)))
-           (when (not ext-js) (setq tide-require-manual-setup t))
+           ;; tsserver requires non-.ts files to be manually added to the files
+           ;; array in tsconfig.json, otherwise the file will be loaded as part
+           ;; of an 'inferred project'. This won't be necessary anymore after
+           ;; TypeScript allows defining custom file
+           ;; extensions. https://github.com/Microsoft/TypeScript/issues/8328
+           (unless ext-js (setq tide-require-manual-setup t))
            (setq rh-js2-additional-externs
                  (append rh-js2-additional-externs '("require" "exports")))
-           ;; (rh-setup-javascript-tide)
-           ))))
+           (tern-mode 1)))))
